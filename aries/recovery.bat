700dcd05132ed60b8f76dfba2b7495fdb4809ed8
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-02-08 18:39:46 +0100

    Update recovery.bat

--- aries/recovery.bat
+++ aries/recovery.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 @adb reboot bootloader
-@fastboot flash recovery C:\ariesTool\recovery\recovery.img
+@fastboot flash recovery C:\XiaomiTool\aries\recovery\recovery.img
 echo Done!
 @fastboot reboot
 echo Rebooting...
