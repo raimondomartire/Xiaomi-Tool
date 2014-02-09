f69518e6d646c6bf84f9a4bee70fad7c98f5ea43
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-02-09 20:52:41 +0100

    Update Record.bat

--- common/Record.bat
+++ common/Record.bat
@@ -4,7 +4,8 @@ adb wait-for-device
 echo Press ctrl + c when u want to stop it
 @adb shell mkdir /sdcard/tmp
 adb shell screenrecord /sdcard/tmp/video.mp4
-@adb pull /sdcard/tmp/video.mp4 C:\LgTool\Pulled\
+@adb pull /sdcard/tmp/video.mp4 C:\XiaomiTool\Pulled\
 @adb shell rm -r /sdcard/tmp
+echo You can find your video inside "C:\XiaomiTool\Pulled" folder
 echo Done!
 pause
