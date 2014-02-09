2ceae9607d5ae6b65837d7fb2f15b9ded3d955f8
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-02-09 20:54:34 +0100

    Update camera.bat

--- common/camera.bat
+++ common/camera.bat
@@ -1,10 +1,10 @@
 @echo off
 cls
 adb wait-for-device
-echo Photos will be pull to C:\LgTool\Pulled\Camera
+echo Photos will be pull to "C:\XiaomiTool\Pulled\Camera"
 echo.
 pause
 echo Copying camera photos...
-@adb pull /sdcard/DCIM/camera/* C:\LgTool\Pulled\Camera\
+@adb pull /sdcard/DCIM/camera/* C:\XiaomiTool\Pulled\Camera\
 echo Done!
 pause 
