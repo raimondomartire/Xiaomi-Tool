4f1d4b86ab53c6da09be92a813f5335b6ad9efda
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-02-09 20:51:02 +0100

    Update Pull.bat

--- common/Pull.bat
+++ common/Pull.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-echo Push a file "to C:\LgTool\Pulled"
+echo Push a file "to C:\XiaomiTool\Pulled"
 SET /P FIDIR= Type here File path (It can NOT contain spaces) :
-adb pull %FIDIR% C:\LgTool\Pulled\
+adb pull %FIDIR% C:\XiaomiTool\Pulled\
 pause
