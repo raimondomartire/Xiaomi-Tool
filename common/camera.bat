7c121677decc85c8f0ca5f9b1ba20c04c8f83a2a
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-02-09 20:57:05 +0100

    Update camera.bat
    Fixed

--- common/camera.bat
+++ common/camera.bat
@@ -5,6 +5,7 @@ echo Photos will be pull to "C:\XiaomiTool\Pulled\Camera"
 echo.
 pause
 echo Copying camera photos...
-@adb pull /sdcard/DCIM/camera/* C:\XiaomiTool\Pulled\Camera\
+@adb pull /sdcard/DCIM/Camera/*.jpg C:\XiaomiTool\Pulled\Camera\
+@adb pull /sdcard/DCIM/Camera/*.mp4 C:\XiaomiTool\Pulled\Camera\
 echo Done!
 pause 
